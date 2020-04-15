457dfe9731b3ad06986fa8bdad4e7b9ae650ab48
Nicky Yim <nicky-yim@users.noreply.github.com>
2020-04-15 16:01:28 -0400

    Removed fuse.js
    Fuse.js doesn't work quite well

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-const Fuse = require('fuse.js');
 const app = express();
 
 const PORT = 3000;
@@ -8,17 +7,24 @@ const PATH = './city.list.min.json';
 var cities;
 
 const buildCities = () => {
+  console.log('Reading', PATH);
+
   fs.promises
     .readFile(PATH, (err, data) => {
       if (err) throw err;
       return data;
     })
-    .then((data) => (cities = JSON.parse(data)));
+    .then((data) => (cities = JSON.parse(data)))
+    .catch((err) => console.error(err));
 };
 buildCities();
 
+app.get('/search/', (req, res) => {
+  res.send([]);
+});
+
 app.get('/search/:query', (req, res) => {
-  res.send(req.params);
+  res.send(req.params.query);
 });
 
 app.get('*', (req, res) => {
